465f68ab003bc0b56c0f1bbbdedd94d027217af6
Wulab324 <79780271+Wulab324@users.noreply.github.com>
2021-03-25 18:56:17 +0800

    Add files via upload

--- plots_and_analysis.py
+++ plots_and_analysis.py
@@ -40,15 +40,15 @@ legend_label = ['AS', 'SH']
 
 # NCBI dataset
 ex = pd.read_csv('results_final/ncbi_kfold.csv')
-lw = 0.10
+lw = 0.25
 ASs = list(ex.diff_score[:300])
 SHs = list(ex.diff_score[300:])
 random.shuffle(ASs)
 random.shuffle(SHs)
-out1 = plt.bar(range(300), ASs, color='blue', linewidth=lw, 
-               edgecolor='blue')
-out2 = plt.bar(range(300,600), SHs, color='red', linewidth=lw, 
-               edgecolor='red')
+out1 = plt.bar(range(300), ASs, color='#00BFFF', linewidth=lw, 
+               edgecolor='#00BFFF')
+out2 = plt.bar(range(300,600), SHs, color='#00FA9A', linewidth=lw, 
+               edgecolor='#00FA9A')
 pltout = [x[0] for x in [out1, out2]]
 
 plt.xlabel('Sequence', **label_font)
@@ -58,9 +58,13 @@ plt.yticks([-300,-150,0,150,300], **ticks_font)
 plt.xlim([-1,601])
 plt.axhline(color='black', linewidth=1)
 plt.legend(pltout, legend_label, prop=legend_font, 
-           loc='upper right')
+           loc='upper right',frameon=False)
 plt.tight_layout()
+plt.rcParams['savefig.dpi'] = 300 
+plt.rcParams['figure.dpi'] = 300 
 plt.savefig('plots/ncbi_kfold.pdf')
+plt.savefig('plots/ncbi_kfold.png',transparent = True)
+plt.savefig('plots/ncbi_kfold.svg',format='svg',transparent = True)
 plt.close()
 
 
@@ -78,22 +82,22 @@ rules_amino = pd.read_csv('results_final/rules/rules_amino.csv', index_col=0)
 rules_type = pd.read_csv('results_final/rules/rules_type.csv', index_col=0)
 
 mcc = list(rules.mcc)
-min_mcc = np.percentile(mcc, 95)  # mcc > 0.73
+min_mcc = np.percentile(mcc, 98)  # mcc > 0.86
 rules_mcc = rules[rules.mcc >= min_mcc]
-rules_amino_mcc = rules_amino[rules_amino.mcc >= min_mcc]  # 72 rules
-rules_type_mcc = rules_type[rules_type.mcc >= min_mcc]  # 33 rules
-positions = sorted(set(rules_mcc.tre_pos))  # 61 positions
+rules_amino_mcc = rules_amino[rules_amino.mcc >= min_mcc]  # 32 rules
+rules_type_mcc = rules_type[rules_type.mcc >= min_mcc]  # 16 rules
+positions = sorted(set(rules_mcc.Np_pos))  # 39 positions
 rules_mcc.to_csv('results_final/rules/rules_mcc.csv')
 rules_amino_mcc.to_csv('results_final/rules/rules_amino_mcc.csv')
 rules_type_mcc.to_csv('results_final/rules/rules_type_mcc.csv')
 
-rules_amino_table = rules_amino_mcc.loc[:,['tre_pos','rule', 'closest_subsite', 
+rules_amino_table = rules_amino_mcc.loc[:,['Np_pos','rule', 'closest_subsite', 
                                            'dist_subsite','sens', 'spec', 'acc', 'mcc']]
 rules_amino_table.columns = ['Position', 'Rule', 'Closest subsite', 
                              'Distance to closest subsite (Å)', 'Sensitivity', 
                              'Specificity', 'Accuracy', 'MCC']
 rules_amino_table.to_csv('plots/rules_amino_table.csv')
-rules_type_table = rules_type_mcc.loc[:,['tre_pos','rule', 'closest_subsite', 
+rules_type_table = rules_type_mcc.loc[:,['Np_pos','rule', 'closest_subsite', 
                                          'dist_subsite', 'sens', 'spec', 'acc', 'mcc']]
 rules_type_table.columns = ['Position', 'Rule', 'Closest subsite', 
                              'Distance to closest subsite (Å)', 'Sensitivity',
@@ -117,14 +121,18 @@ plt.rcParams['grid.alpha'] = 0.5
 plt.rcParams['axes.axisbelow'] = True
 
 weights = np.zeros_like(mcc) + 1/len(mcc)
-plt.hist(mcc, bins=12, rwidth=1, color='darkgreen', weights=weights)
-plt.xticks(np.arange(-80,101,40)*0.01, **ticks_font)
+plt.hist(mcc, bins=12, rwidth=1, color='royalblue', weights=weights)
+plt.xticks(np.arange(-60,101,40)*0.01, **ticks_font)
 plt.yticks(np.arange(0,28,5)*0.01, **ticks_font)
 plt.xlabel('MCC', **label_font)
 plt.ylabel('Relative frequency', **label_font)
 plt.tight_layout()
+plt.rcParams['savefig.dpi'] = 300 
+plt.rcParams['figure.dpi'] = 300 
+plt.savefig('plots/rules_mcc_dist.png',transparent = True)
+plt.savefig('plots/rules_mcc_dist.svg',format='svg',transparent = True)
 plt.savefig('plots/rules_mcc_dist.pdf')
-
+plt.close()
 
 
 
@@ -132,18 +140,27 @@ plt.savefig('plots/rules_mcc_dist.pdf')
 
 # Minimum distance between rules' positions and substrate
 #============================================================#
+fnt='Arial'
+ticks_font = {'fontname':fnt, 'size':'20'}
+label_font = {'family':fnt, 'size':'22'}
+title_font = {'family':fnt, 'size':'24'}
+plt.rcParams['figure.figsize'] = [6,3.5]
+plt.rcParams['grid.alpha'] = 0.5
+plt.rcParams['axes.axisbelow'] = True
 
-dist50 = np.percentile(rules_mcc.dist_subsite, 50) #4.79Å
+
+#dist58 = np.percentile(rules_mcc.dist_subsite, 58) #4.79Å
 rule_dist = list(rules_mcc.dist_subsite)
 weights = np.zeros_like(rule_dist) + 1/len(rule_dist)
-plt.hist(rule_dist, bins=7, weights=weights, color='indigo')
-plt.xticks(np.arange(0,30,5), **ticks_font)
-plt.xlim((0,25))
-plt.yticks(**ticks_font)
+plt.hist(rule_dist, bins=10, weights=weights, color='royalblue')
+plt.xticks(np.arange(0,31,5), **ticks_font)
+plt.xlim((0,30))
+plt.yticks(np.arange(0,76,25)*0.01, **ticks_font)
 plt.xlabel('Distance to substrate (Å)', **label_font)
 plt.ylabel('Relative frequency', **label_font)
 plt.tight_layout()
 plt.savefig('plots/rules_distance_dist.pdf')
+plt.close()
 
 
 
@@ -152,8 +169,7 @@ plt.savefig('plots/rules_distance_dist.pdf')
 
 
 
-
-# Distribution at 61 important positions
+# Distribution at 39 important positions
 #==========================================#
 
 plt.rcParams['figure.figsize'] = [7,4]
@@ -172,20 +188,20 @@ for i in range(len(positions)):
 GH13msa.get_consensus_sequences()
 AS_consensus = list(GH13msa.consensus_AS)
 SH_consensus = list(GH13msa.consensus_SH)
-tre = bioinf.split_fasta('fasta/GH13_positions_only/consensus.fasta')[1][1]
+Np = bioinf.split_fasta('fasta/GH13_positions_only/consensus.fasta')[1][1]
 excel = pd.read_csv('results_final/residue_distances.csv', index_col=0)
 closest_subsite = list(excel.iloc[:,0])
 distances = list(excel.iloc[:,1])
 
-resid_aro, tre_aro, AS_aro, SH_aro, closest_subsite_aro, dist_aro = [],[],[],[],[],[]
+resid_aro, Np_aro, AS_aro, SH_aro, closest_subsite_aro, dist_aro = [],[],[],[],[],[]
 AS_aro_freq, SH_aro_freq, conserved = [], [], []
 aro_res = ['F', 'W', 'Y', 'H']
 
-for i in range(len(tre)):
-    if (tre[i] in aro_res or AS_consensus[i] in  aro_res or SH_consensus[i] in aro_res)\
+for i in range(len(Np)):
+    if (Np[i] in aro_res or AS_consensus[i] in  aro_res or SH_consensus[i] in aro_res)\
     and distances[i]<=6.0:
         resid_aro.append(i+1)
-        tre_aro.append(tre[i])
+        Np_aro.append(Np[i])
         AS_aro.append(AS_consensus[i])
         SH_aro.append(SH_consensus[i])
         closest_subsite_aro.append(closest_subsite[i])
@@ -198,12 +214,12 @@ for i in range(len(tre)):
             conserved.append('AS')
         elif AS_freq < 66 and SH_freq > 66:
             conserved.append('SH')
-        elif AS_freq > 49 and SH_freq > 49:
+        elif AS_freq > 66 and SH_freq > 66:
             conserved.append('AS and SH')
         else:
             conserved.append('None')
 
-store = pd.DataFrame([resid_aro, tre_aro, AS_aro, SH_aro, AS_aro_freq, SH_aro_freq, 
+store = pd.DataFrame([resid_aro, Np_aro, AS_aro, SH_aro, AS_aro_freq, SH_aro_freq, 
                       closest_subsite_aro, dist_aro, conserved]).transpose()
 store.columns = ['Position', 'GH13 residue', 'AS consensus residue', 
                  'SH consensus residue', 'Frequency of aromatic residues in ASs (%)', 
@@ -231,3 +247,68 @@ with open('plots/aromatic_pymol.txt', 'w') as pym:
     pym.write(pymol_AS[:-4] + '\n\n')
     pym.write(pymol_both[:-4] + '\n\n')
 
+
+# Table of position-specific rules for predicting hydrolysis or  transglycosylation
+#======================================================#
+ex = pd.read_csv('results_final/ml_rf_pred/position_rules.csv', index_col=0)
+table = pd.DataFrame()
+table['position'] = [int(x.split()[-1].split('=>')[0][1:]) for x in ex.rule]
+table['rule'] = ex.rule
+table['sensitivity'] = [round(x, 1) for x in ex.sensitivity]
+table['specificity'] = [round(x,1) for x in ex.specificity]
+table['accuracy'] = [round(x,1) for x in ex.accuracy]
+table['mcc'] = [round(x,2) for x in ex.mcc]
+table = table.sort_values('position', ascending=True)
+table.to_csv('plots/SH_rules.csv')
+
+
+# Plot of feature importances of top 20 features
+#================================================#
+
+ex = pd.read_csv('results_final/ml_rf_pred/featimp_top20.csv', index_col=0)
+ex = ex.sort_values('mean', ascending=False)
+
+fnt='Arial'
+ticks_font = {'fontname':fnt, 'size':'16'}
+label_font = {'family':fnt, 'size':'20'}
+plt.rcParams['figure.figsize'] = [6,3]
+
+plt.bar(range(len(ex)), ex.iloc[:,1], color='royalblue', linewidth=0.6, edgecolor='black',
+        yerr=ex.iloc[:,2], ecolor='black', capsize=3)
+plt.xticks(range(len(ex)), ex.iloc[:,0], **ticks_font, rotation=90)
+plt.yticks(**ticks_font)
+plt.xlabel('Features', **label_font)
+plt.ylabel('Gini importance', **label_font)
+plt.tight_layout()
+plt.rcParams['savefig.dpi'] = 300 
+plt.rcParams['figure.dpi'] = 300 
+plt.savefig('plots/H_top20_featimp.png',transparent = True)
+plt.savefig('plots/H_top20_featimp.svg',format='svg',transparent = True)
+plt.savefig('plots/H_top20_featimp.pdf')
+plt.close()
+
+
+
+# Plot feature importances for SH prediction (All 7472 features)
+#===============================================================================#
+
+ex = pd.read_csv('results_final/ml_rf_pred/featimp_all.csv', index_col=0)
+ex = ex.sort_values('mean', ascending=False)
+
+fnt='Arial'
+ticks_font = {'fontname':fnt, 'size':'16'}
+label_font = {'family':fnt, 'size':'20'}
+plt.rcParams['figure.figsize'] = [6,3]
+
+plt.bar(range(len(ex)), list(ex.iloc[:,1]), color='royalblue', linewidth=1, edgecolor='royalblue')
+plt.xticks(**ticks_font)
+plt.yticks(**ticks_font)
+plt.xlabel('Features', **label_font)
+plt.ylabel('Gini importance', **label_font)
+plt.tight_layout()
+plt.rcParams['savefig.dpi'] = 300 
+plt.rcParams['figure.dpi'] = 300 
+plt.savefig('plots/H_featimp_all.png',transparent = True)
+plt.savefig('plots/H_featimp_all.svg',format='svg',transparent = True)
+plt.savefig('plots/H_featimp_all.pdf')
+plt.close()
---
+++ subtype_rf.py
@@ -0,0 +1,354 @@
+"""
+Discriminating GH13 ASs and SHs with Random Forest.
+"""
+
+
+
+
+# Imports
+#=====================#
+import pandas as pd
+import numpy as np
+from scipy import stats
+import random
+from Bio import SeqIO
+import os
+import subprocess
+
+from imblearn.under_sampling import RandomUnderSampler
+
+from Bio.Blast.Applications import NcbiblastpCommandline
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import KFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import bioinformatics as bioinf
+
+
+
+# Prepare sequences and data
+#=====================================================#
+accession_S = []
+parser= SeqIO.parse("fasta/GH13_positions_only/GH13_all.fasta", "fasta")
+for sequence in parser:
+    if sequence.seq[294:295]=="S":
+        accession_S.append(sequence.id)
+
+accession_all = bioinf.get_accession('fasta/initial_blast/nrblast_all.fasta')
+has_S = [1 if x in accession_S else 0 for x in accession_all]
+df = pd.DataFrame([accession_all, has_S], index=['accession', 'has_S']).transpose()
+df.to_csv('results_final/has_S.csv')
+
+
+# S distribution
+df['subtype'] = pd.read_csv('results_final/ncbi_subtypes.csv')['ncbi_pred_class']
+df_AS = df[df['subtype']==1]
+df_SH = df[df['subtype']==0]
+
+AS_noS = df_AS.has_S.value_counts()[0]
+SH_S = df_SH.has_S.value_counts()[1]
+
+
+
+# Derive features for machine learning with one-hot encoding
+#============================================================#
+cat_domain_fasta = 'fasta/GH13_positions_only/GH13_cat.fasta'
+sequence_df = bioinf.fasta_to_df(cat_domain_fasta)
+X_features = pd.DataFrame() # empty dataframe for storing features
+
+for i in range(len(sequence_df.columns)):
+    # Convert amino acids to integers
+    X_resid = list(sequence_df.iloc[:,i])
+    labelencoder = LabelEncoder()
+    X_label = list(labelencoder.fit_transform(X_resid))
+    X_resid_unique = sorted(set(X_resid))
+    X_label_unique = sorted(set(X_label))
+    
+    # Map integer labels to amino acids
+    label_resid = [X_label.index(num) for num in X_label_unique]
+    label_resid = [X_resid[num] for num in label_resid]
+    
+    # Convert labels to binary features (one-hot encoding)
+    onehotencoder = OneHotEncoder()
+    X_label = pd.DataFrame(X_label) # convert to 2D array
+    X_encoded = onehotencoder.fit_transform(X_label).toarray()
+    X_encoded = pd.DataFrame(X_encoded)
+    
+    # Name encoded features (residue + position, e.g G434)
+    X_encoded.columns = ['{0}{1}'.format(res,i+1) for res in label_resid]
+    del X_encoded['-{0}'.format(i+1)]  # remove encoded features from gaps
+    
+    # Append features to dataframe store
+    for col in X_encoded.columns:
+        X_features[col] = X_encoded[col]    
+
+		
+# Randomly split data to validation set and test set
+#====================================================#
+y = pd.Series(has_S)   # class labels
+y_yes_S = y[y==1]  
+y_no_S = y[y==0]
+
+# Test set data (10% of total data)
+yes_test_size = int(0.1 * len(y_yes_S))
+no_test_size = int(0.1 * len(y_no_S))
+yes_test_indices = random.sample(list(y_yes_S.index), yes_test_size)
+no_test_indices = random.sample(list(y_no_S.index), no_test_size)
+test_indices = yes_test_indices + no_test_indices
+test_indices = sorted(test_indices)
+
+# Validation set data (90% of total data)
+val_indices = [x for x in list(y.index) if x not in test_indices]
+
+# X (features) and y for validation and test sets
+X_val = X_features.iloc[val_indices,:]
+y_val = y.iloc[val_indices]
+X_test_sep = X_features.iloc[test_indices,:]
+y_test_sep = y.iloc[test_indices]
+
+
+
+
+
+
+# Apply random forests to validation set using all features
+#=============================================================#
+
+# Empty lists for storing final results
+sens_store, spec_store, acc_store, mcc_store, featimp_store = [], [], [], [], []
+
+# Function for evaluating performance
+def evalPerf(y_test, y_pred):
+    '''Return (sensitivity, specificity, accuracy, MCC, p_value)'''
+    cm = confusion_matrix(y_test, y_pred)
+    tn, tp, fn, fp = cm[0][0], cm[1][1], cm[1][0], cm[0][1]
+    n = tp + fp + tn + fn
+    accuracy = (tp + tn)/n * 100
+    mcc = ((tp*tn) - (fp*fn))/np.sqrt((tp+fp)*(tn+fn)*(tp+fp)*(tn+fp))
+    sens = tp/(tp + fn) * 100 if tp + fp != 0 else 0
+    spec = tn/(tn + fp) * 100 if tn + fn != 0 else 0
+    table = np.array([[tp, fp], [fn, tn]]) # AS and SH have same contingency table
+    p_value = stats.chi2_contingency(table)[1]
+    return [sens, spec, accuracy, mcc, p_value]
+
+
+# 100 repetitions of 10-fold cross validation
+for r in range(100):
+    RUS = RandomUnderSampler(random_state=None)
+    X_select, y_select = RUS.fit_resample(X_val, y_val)
+    X_select, y_select = pd.DataFrame(X_select), pd.Series(y_select)
+    
+    # 10-fold cross validation
+    kf = KFold(n_splits=10, shuffle=True, random_state=None)
+    kf_indices = kf.split(X_select)
+    for train_index, test_index in kf_indices:
+        X_train, y_train = X_select.iloc[train_index, :], y_select.iloc[train_index]
+        X_test, y_test = X_select.iloc[test_index, :], y_select.iloc[test_index]
+        
+        # Fit random forest classifier to training data
+        classifier = RandomForestClassifier(n_estimators=800, n_jobs=-1)
+        classifier.fit(X_train, y_train)
+        
+        # Test classifier and evaluate performance
+        y_pred = classifier.predict(X_test)
+        sens, spec, accuracy, mcc, pvalue = evalPerf(y_test, y_pred)
+        featimp = list(classifier.feature_importances_)
+        
+        # Save results
+        sens_store.append(sens)
+        spec_store.append(spec)
+        acc_store.append(accuracy)
+        mcc_store.append(mcc)
+        featimp_store.append(featimp)
+
+# Average results over all 500 repetitions
+store = [np.mean(sens_store), np.std(sens_store), np.mean(spec_store), np.std(spec_store),
+         np.mean(acc_store), np.std(acc_store), np.mean(mcc_store), np.std(mcc_store)]
+store = pd.DataFrame(store, index=['sens_mean', 'sens_std', 'spec_mean', 'spec_std',
+                                   'acc_mean', 'acc_std', 'mcc_mean', 'mcc_std'])
+featimp_mean = pd.DataFrame(featimp_store).mean(axis=0)
+featimp_std = pd.DataFrame(featimp_store).std(axis=0)
+store_featimp = pd.DataFrame([X_val.columns, featimp_mean, featimp_std], 
+                             index=['features', 'mean', 'std']).transpose()
+
+# Write results to spreadsheet
+store.to_csv('results_final/ml_rf_pred/perf_all.csv')
+store_featimp.to_csv('results_final/ml_rf_pred/featimp_all.csv')
+     
+   
+        
+
+
+        
+# Use only top 50 features
+#===================================#
+
+# Top 50 features
+top50_index = list(store_featimp.sort_values(by='mean', ascending=False).iloc[:50,:].index)
+X_val_top50 = X_val.iloc[:,top50_index]
+
+# Empty lists for storing final results
+sens_store, spec_store, acc_store, mcc_store, featimp_store = [], [], [], [], []
+
+# 100 repetitions of 10-fold cross validation
+for r in range(100):
+    RUS = RandomUnderSampler(random_state=None)
+    X_select, y_select = RUS.fit_resample(X_val_top50, y_val)
+    X_select, y_select = pd.DataFrame(X_select), pd.Series(y_select)
+    
+    # 10-fold cross validation
+    kf = KFold(n_splits=10, shuffle=True, random_state=None)
+    kf_indices = kf.split(X_select)
+    for train_index, test_index in kf_indices:
+        X_train, y_train = X_select.iloc[train_index, :], y_select.iloc[train_index]
+        X_test, y_test = X_select.iloc[test_index, :], y_select.iloc[test_index]
+        
+        # Fit random forest classifier to training data
+        classifier = RandomForestClassifier(n_estimators=800, n_jobs=-1)
+        classifier.fit(X_train, y_train)
+        
+        # Test classifier and evaluate performance
+        y_pred = classifier.predict(X_test)
+        sens, spec, accuracy, mcc, pvalue = evalPerf(y_test, y_pred)
+        featimp = list(classifier.feature_importances_)
+        
+        # Save results
+        sens_store.append(sens)
+        spec_store.append(spec)
+        acc_store.append(accuracy)
+        mcc_store.append(mcc)
+        featimp_store.append(featimp)
+
+# Average results over all 500 repetitions
+store_top50 = [np.mean(sens_store), np.std(sens_store), np.mean(spec_store), np.std(spec_store),
+         np.mean(acc_store), np.std(acc_store), np.mean(mcc_store), np.std(mcc_store)]
+store_top50 = pd.DataFrame(store_top50, index=['sens_mean', 'sens_std', 'spec_mean', 'spec_std',
+                                   'acc_mean', 'acc_std', 'mcc_mean', 'mcc_std'])
+featimp_mean_top50 = pd.DataFrame(featimp_store).mean(axis=0)
+featimp_std_top50 = pd.DataFrame(featimp_store).std(axis=0)
+store_featimp_top50 = pd.DataFrame([X_val_top50.columns, featimp_mean_top50, featimp_std_top50], 
+                             index=['features', 'mean', 'std']).transpose()
+
+# Write results to spreadsheet
+store_top50.to_csv('results_final/ml_rf_pred/perf_top50.csv')
+store_featimp_top50.to_csv('results_final/ml_rf_pred/featimp_top50.csv')
+
+
+# Use only top 20 features
+#=================================#
+
+# Top 20 features
+top20_index = list(store_featimp.sort_values(by='mean', ascending=False).iloc[:20,:].index)
+X_val_top20 = X_val.iloc[:,top20_index]
+
+# Empty lists for storing final results
+sens_store, spec_store, acc_store, mcc_store, featimp_store = [], [], [], [], []
+
+# 100 repetitions of 10-fold cross validation
+for r in range(100):
+    RUS = RandomUnderSampler(random_state=None)
+    X_select, y_select = RUS.fit_resample(X_val_top20, y_val)
+    X_select, y_select = pd.DataFrame(X_select), pd.Series(y_select)
+    
+    # 5-fold cross validation
+    kf = KFold(n_splits=10, shuffle=True, random_state=None)
+    kf_indices = kf.split(X_select)
+    for train_index, test_index in kf_indices:
+        X_train, y_train = X_select.iloc[train_index, :], y_select.iloc[train_index]
+        X_test, y_test = X_select.iloc[test_index, :], y_select.iloc[test_index]
+        
+        # Fit random forest classifier to training data
+        classifier = RandomForestClassifier(n_estimators=800, n_jobs=-1)
+        classifier.fit(X_train, y_train)
+        
+        # Test classifier and evaluate performance
+        y_pred = classifier.predict(X_test)
+        sens, spec, accuracy, mcc, pvalue = evalPerf(y_test, y_pred)
+        featimp = list(classifier.feature_importances_)
+        
+        # Save results
+        sens_store.append(sens)
+        spec_store.append(spec)
+        acc_store.append(accuracy)
+        mcc_store.append(mcc)
+        featimp_store.append(featimp)
+
+# Average results over all 500 repetitions
+store_top20 = [np.mean(sens_store), np.std(sens_store), np.mean(spec_store), np.std(spec_store),
+         np.mean(acc_store), np.std(acc_store), np.mean(mcc_store), np.std(mcc_store)]
+store_top20 = pd.DataFrame(store_top20, index=['sens_mean', 'sens_std', 'spec_mean', 'spec_std',
+                                   'acc_mean', 'acc_std', 'mcc_mean', 'mcc_std'])
+featimp_data = pd.DataFrame(featimp_store, columns=X_val_top20.columns)
+featimp_mean_top20 = pd.DataFrame(featimp_store).mean(axis=0)
+featimp_std_top20 = pd.DataFrame(featimp_store).std(axis=0)
+store_featimp_top20 = pd.DataFrame([X_val_top20.columns, featimp_mean_top20, featimp_std_top20], 
+                             index=['features', 'mean', 'std']).transpose()
+
+# Write results to excel spreadsheet
+featimp_data.to_csv('results_final/ml_rf_pred/featimp_top20_fulldata.csv')
+store_top20.to_csv('results_final/ml_rf_pred/perf_top20.csv')
+store_featimp_top20.to_csv('results_final/ml_rf_pred/featimp_top20.csv')
+        
+
+
+
+# Train top20 classifier and test on test set
+#===============================================#
+RUS = RandomUnderSampler(random_state=None)
+X_select, y_select = RUS.fit_resample(X_val_top20, y_val)
+classifier = RandomForestClassifier(n_estimators=800, n_jobs=-1)
+classifier.fit(X_select, y_select)
+y_pred = classifier.predict(X_test_sep.iloc[:,top20_index])
+cm = confusion_matrix(y_test_sep, y_pred)
+tn, tp, fn, fp = cm[0][0], cm[1][1], cm[1][0], cm[0][1]
+sens, spec, accuracy, mcc, pvalue = evalPerf(y_test_sep, y_pred)
+store = pd.DataFrame([tp, fp, tn, fn, sens, spec, accuracy, mcc], 
+                     index=['tp', 'fp', 'tn', 'fn', 'sens', 'spec', 'acc', 'mcc'])
+store.to_csv('results_final/ml_rf_pred/perf_test_set.csv')
+
+        
+
+
+
+
+# Position specific rules from top 50 features (Supporting Information)
+#===============================================================================#
+store = [] # empty list for storing results
+for col in X_val_top50.columns:
+    # Test the rule, feature => SH
+    y_pred = X_val_top50[col]
+    cm = confusion_matrix(y_val, y_pred)
+    perf = evalPerf(y_val, y_pred)
+    
+    # Test the rule, not feature => SH
+    y_pred1 = [1 if x==0 else 0 for x in y_pred]
+    cm1 = confusion_matrix(y_val, y_pred1)
+    perf1 = evalPerf(y_val, y_pred1)
+    
+    # Add rule (i.e. the rule with the highest MCC)
+    if perf[3] > perf1[3]:
+        perf = ['{0}=>SH'.format(col)] + perf
+        store.append(perf)
+    else:
+        perf1 = ['not {0}=>SH'.format(col)] + perf1
+        store.append(perf1)
+
+store = pd.DataFrame(store, columns=['rule', 'sensitivity', 
+                                     'specificity', 'accuracy', 'mcc', 'pvalue'])
+store.to_csv('results_final/ml_rf_pred/position_rules.csv')
+
+    
+    
+    
+
+
+
+
+
+
